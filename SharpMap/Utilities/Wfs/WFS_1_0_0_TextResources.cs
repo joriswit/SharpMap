0304f178113312867efb91e1a0f4699c968bec19
joriswit <github@joriswit.nl>
2017-07-11 21:56:27 +0200

    WFS fixes: - Namespace URL was not correctly URL-encoded - In WFS 1.1.0, SRS URL component is named SRSNAME
    # Conflicts:
    #	SharpMap/Utilities/Wfs/WFS_1_0_0_TextResources.cs
    #	SharpMap/Utilities/Wfs/WFS_1_1_0_TextResources.cs

--- SharpMap/Utilities/Wfs/WFS_1_0_0_TextResources.cs
+++ SharpMap/Utilities/Wfs/WFS_1_0_0_TextResources.cs
@@ -1,6 +1,7 @@
 // WFS provider by Peter Robineau (peter.robineau@gmx.at)
 // This file can be redistributed and/or modified under the terms of the GNU Lesser General Public License.
 
+using System;
 using System.IO;
 using System.Text;
 using System.Xml;
@@ -67,7 +68,7 @@ namespace SharpMap.Utilities.Wfs
             }
 
             StringBuilder filterBuilder = new StringBuilder();
-            filterBuilder.Append("&filter=%3CFilter%20xmlns=%22" + NSOGC + "%22%20xmlns:gml=%22" + NSGML +
+            filterBuilder.Append("&filter=%3CFilter%20xmlns=%22" + Uri.EscapeDataString(NSOGC) + "%22%20xmlns:gml=%22" + Uri.EscapeDataString(NSGML) +
                                  "%22%3E%3CBBOX%3E%3CPropertyName%3E");
             filterBuilder.Append(qualification);
             filterBuilder.Append(featureTypeInfo.Geometry._GeometryName);
